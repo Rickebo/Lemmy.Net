acda028beaa42f036690ae5fde855efa29798f8b
Simon Pettersson <rickebo@rickebo.com>
2023-07-02 18:27:50 +0200

    Added example project to list communities for instance

---
+++ Examples/ListCommunities/Program.cs
@@ -0,0 +1,9 @@
+﻿using Lemmy.Net.Types;
+using LemmyApi;
+
+var api = new LemmyHttp(
+    Environment.GetEnvironmentVariable("LEMMY_API_URL") ?? "https://lemmy.ml"
+);
+
+await foreach (var community in api.ListAllCommunities(new ListCommunities()))
+    Console.WriteLine(community.Community.Name + "@" + new Uri(community.Community.ActorId).Host);
