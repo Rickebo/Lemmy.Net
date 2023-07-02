84b87d55df73b106fea11ac30f057ee21c2b54ee
Simon Pettersson <rickebo@rickebo.com>
2023-07-02 17:48:55 +0200

    Added example project to list federated instances

---
+++ Examples/ListFederatedInstances/Program.cs
@@ -0,0 +1,27 @@
+﻿using LemmyApi;
+
+var api = new LemmyHttp(
+    Environment.GetEnvironmentVariable("LEMMY_API_URL") ?? "https://lemmy.ml"
+);
+
+var instances = await api.GetFederatedInstances();
+const string indent = "    ";
+
+if (instances != null)
+{
+    var types = new[]
+    {
+        ("Linked", instances.FederatedInstances.Linked),
+        ("Allowed", instances.FederatedInstances.Allowed),
+        ("Blocked", instances.FederatedInstances.Blocked)
+    };
+
+    foreach (var (type, typeInstances) in types)
+    {
+        Console.WriteLine($"{type} instances:");
+        foreach (var instance in instances.FederatedInstances.Linked)
+            Console.WriteLine($"{indent}{instance.Domain}");
+
+        Console.WriteLine();
+    }
+}
