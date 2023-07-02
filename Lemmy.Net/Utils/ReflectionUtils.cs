cb9ce1dca1ab928f3d5a110e8883f4a64e343c26
Simon Pettersson <rickebo@rickebo.com>
2023-07-02 17:43:14 +0200

    Fixed faulty URL generation for query requests

--- Lemmy.Net/Utils/ReflectionUtils.cs
+++ Lemmy.Net/Utils/ReflectionUtils.cs
@@ -1,31 +1,67 @@
 ﻿using System.Reflection;
+using System.Text.Json.Serialization;
 using System.Web;
 
 namespace LemmyApi.Utils;
 
 public static class ReflectionUtils
 {
-    private static readonly ThreadLocal<Dictionary<Type, PropertyInfo[]>> PropertyCache =
+    private static readonly ThreadLocal<Dictionary<Type, TypeCache>> PropertyCache =
         new(
-            () => new Dictionary<Type, PropertyInfo[]>()
+            () => new Dictionary<Type, TypeCache>()
         );
 
     public static string? ToQueryString<TBody>(TBody body)
     {
         var type = typeof(TBody);
-        
-        var properties = PropertyCache.Value!.TryGetValue(type, out var props)
+
+        var typeCache = PropertyCache.Value!.TryGetValue(type, out var props)
             ? props
-            : PropertyCache.Value[type] = type.GetProperties(BindingFlags.Public);
+            : PropertyCache.Value[type] = new TypeCache(type);
 
         var queryAttributes = HttpUtility.ParseQueryString("");
 
-        foreach (var property in properties)
+        foreach (var property in typeCache.Properties)
+        {
+            var value = property.GetValue(body);
+            if (value == null)
+                continue;
+
+            var propertyName = typeCache.GetPropertyName(property);
+
             queryAttributes.Add(
-                property.Name,
-                Convert.ToString(property.GetValue(body))
+                propertyName,
+                Convert.ToString(value)
             );
+        }
+
 
         return queryAttributes.ToString();
+    }
+
+    private class TypeCache
+    {
+        public PropertyInfo[] Properties { get; }
+        public Dictionary<PropertyInfo, string> PropertyNames { get; }
+
+        public TypeCache(IReflect type)
+        {
+            Properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            PropertyNames = Properties.ToDictionary(
+                prop => prop,
+                prop =>
+                {
+                    var jsonPropertyName = prop.GetCustomAttribute<JsonPropertyNameAttribute>();
+                    return jsonPropertyName != null
+                        ? jsonPropertyName.Name
+                        : prop.Name;
+                }
+            );
+        }
+
+        public string GetPropertyName(PropertyInfo property) =>
+            PropertyNames.TryGetValue(property, out var name)
+                ? name
+                : property.Name;
     }
 }
--- Lemmy.Net/Utils/UriUtils.cs
+++ Lemmy.Net/Utils/UriUtils.cs
@@ -5,25 +5,43 @@ public static class UriUtils
     public static Uri GetUri<TContent>(TContent content, params string[] baseUrl)
     {
         var parts = (IEnumerable<string>)baseUrl;
+        var uri = GetUri(parts);
+        
         var queryString = GetQueryString(content);
 
-        if (queryString != null)
-            parts = parts.Concat(new[] { queryString });
+        if (queryString == null)
+            return uri;
         
-        return GetUri(parts);
+        if (!queryString.StartsWith('?'))
+            queryString = "?" + queryString;
+
+        var uriString = uri.ToString();
+        if (uriString.EndsWith("/"))
+            uriString = uriString[..^1];
+        
+        return new Uri(uriString + queryString);
     }
 
     public static Uri GetUri(params string?[] parts) =>
         GetUri((IEnumerable<string>)parts);
 
-    public static Uri GetUri(IEnumerable<string> parts) =>
+    public static Uri Append(this Uri uri, string part)
+    {
+        var str = uri.ToString();
+        if (!str.EndsWith("/"))
+            str += "/";
+        
+        return new Uri(str + part);
+    }
+    
+    public static Uri GetUri(IEnumerable<string?> parts) =>
         parts
             .Where(part => part != null)!
             .Aggregate<string, Uri?>(
                 null, (current, part) =>
                     current == null
                         ? new Uri(part)
-                        : new Uri(current, part)
+                        : current.Append(part)
             ) ??
         throw new ArgumentException("No valid URI parts were provided.");
 
